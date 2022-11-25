25658f3013749604eb4372d96566cccb4e851e53
leandroteixeira97 <leandro.teixeira.sjc@gmail.com>
2022-11-25 07:42:06 -0300

    Updating UML Diagram

--- Engenharia-de-Software-I/videostore/src/br/com/leandroteixeira/view/Main.java
+++ Engenharia-de-Software-I/videostore/src/br/com/leandroteixeira/view/Main.java
@@ -2,6 +2,7 @@ package br.com.leandroteixeira.view;
 
 import br.com.leandroteixeira.controller.ConnectionFactory;
 import br.com.leandroteixeira.controller.Utils;
+import br.com.leandroteixeira.model.Movie;
 import br.com.leandroteixeira.model.VideoStore;
 
 public class Main {
@@ -9,6 +10,10 @@ public class Main {
     public static void main(String[] args) {
 
         VideoStore videoStore = new VideoStore(Utils.getMovies(), Utils.getCustomers());
+
+        Movie newMovie = new Movie(0, "Filme de teste", "Categoria de teste", "Sinopse de teste", 2.0, 2022, "Ator de teste 1, Ator de teste 2", 10, 5, 5, true);
+
+        videoStore.
 
     }
 
